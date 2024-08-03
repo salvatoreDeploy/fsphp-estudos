dbee42b95600b4f406524e46b604c538247e3ce2
salvatoreDeploy <liderhenrique@gmail.com>
2024-08-02 23:01:08 -0300

    Aula 02-04

---
+++ 02-iniciando-do-zero-com-php/02-04-variaves-e-tipos-de-dados/README.md
@@ -0,0 +1,17 @@
+# Conceitos e Estudo
+
+## Variaveis
+
+* Dentro do PHP a criação de recurso segue de cima para baixo então antes de usar uma variavel ela deve ser criada;
+* Recurso de variavel variavel, onde usamos $$ com mesmo nome de uma variavel, mas para expor seu valor usamo o valor da variavel normal, assim concatenamos valores em sequencia;
+* Podemos referenciar os valores de uma variavel sendo exemplo o valor de B ser igual o de A, ou o valor que B receber tambem ser refletido no valor A, usando "&" para referencia;
+
+## Boleano
+
+* Valores de verdadeiro e falso, para testagem de algo;
+* Algumas variaveis vao retornar tambem verdadeiro ou falso, mas existem casos que mesmo preenchidas com certos valores ela vai retornar falso, exemplo: array vazio, strig vazia, 0, 0.0;
+
+## Callback
+
+* E uma variavel que recebe uma função que retorna uma execução de algo;
+* Temos tambem as closures, que é uma função anonima que recebe um callback;
--- 02-iniciando-do-zero-com-php/02-04-variaves-e-tipos-de-dados/index.php
+++ 02-iniciando-do-zero-com-php/02-04-variaves-e-tipos-de-dados/index.php
@@ -1,6 +1,8 @@
 <?php
+
 require __DIR__ . '/../../fullstackphp/fsphp.php';
 fullStackPHPClassName("02.04 - Variáveis e tipos de dados");
+
 
 /**
  * [tipos de dados] https://php.net/manual/pt_BR/language.types.php
@@ -8,20 +10,86 @@ fullStackPHPClassName("02.04 - Variáveis e tipos de dados");
  */
 fullStackPHPClassSession("variáveis", __LINE__);
 
+$userFirstName = "Henrique";
+$userLastName = "Araujo";
+$userAge = "33";
+
+echo "<p>{$userFirstName} {$userLastName} <span class='tag'>{$userAge}</span></p>";
+
+$company = "UpInside";
+$$company = "Treinamento";
+
+echo "<h3>{$company} {$UpInside}</h3>";
+
+$calcA = 10;
+$calcB = 20;
+$calcB = &$calcA;
+$calcB = 100;
+
+var_dump([
+    "A" => $calcA,
+    "B" => $calcB
+]);
 
 /**
  * [ tipo boleano ] true | false
  */
 fullStackPHPClassSession("tipo boleano", __LINE__);
 
+$true = true;
+$false = false;
+
+var_dump($true, $false);
+
+$bestAge = ($userAge > 50);
+var_dump($bestAge);
+
+$a = 0;
+$b = 0.0;
+$c = ""; // ou ''
+$d = array(); // ou []
+$e = null;
+
+var_dump($a, $b, $c, $d, $e);
+
+if($a || $b || $c || $d || $e){
+    var_dump(true);
+}else{
+    var_dump(false);
+}
 
 /**
  * [ tipo callback ] call | closure
  */
 fullStackPHPClassSession("tipo callback", __LINE__);
 
+$code = "<article>Uma callback function!</article>";
+$codeClear = call_user_func("strip_tags", $code);
+var_dump($code, $codeClear);
+
+$codeMore = function($code){
+    var_dump($code);
+};
+$codeMore('#Boracodar');
 
 /**
  * [ outros tipos ] string | array | objeto | numérico | null
  */
 fullStackPHPClassSession("outros tipos", __LINE__);
+
+$string = "Olá mundo";
+$array = [$string];
+$object = new stdClass();
+$object->hello = $string;
+$null = null;
+$int = 123456;
+$float = 1.23456;
+
+var_dump([
+    $string,
+    $array,
+    $object,
+    $null,
+    $int,
+    $float
+]);
