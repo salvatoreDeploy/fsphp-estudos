fb371c032630709f2db4c1153e7c18a24f618a30
salvatoreDeploy <liderhenrique@gmail.com>
2024-08-26 22:44:20 -0300

    modulo-03-04

---
+++ 03-manipulacao-e-tratamento/03-04-manipulacao-de-objetos/README.md
@@ -0,0 +1,4 @@
+# Conceitos e Estudo
+
+- Em uma conversão de array vemos, que as "keys" de um array se transforma em "atributos" para o meu objeto;
+- Podemo criar um objeto usando o (object) ou new StdClass()
--- 03-manipulacao-e-tratamento/03-04-manipulacao-de-objetos/index.php
+++ 03-manipulacao-e-tratamento/03-04-manipulacao-de-objetos/index.php
@@ -7,8 +7,53 @@ fullStackPHPClassName("03.04 - Manipulação de objetos");
  */
 fullStackPHPClassSession("manipulação", __LINE__);
 
+$arrProfile = [
+    "name"=> "Henrique",
+    "company" => "Well",
+    "mail" => "henrique@email.com"
+];
+
+$objProfile = (object)$arrProfile;
+var_dump($arrProfile, $objProfile);
+
+echo "<p>{$arrProfile['name']} trabalha na {$arrProfile['company']}</p>";
+echo "<p>{$objProfile->name} trabalha na {$objProfile->company}</p>";
+
+$ceo = $objProfile;
+unset($ceo->company);
+var_dump($ceo);
+
+$company = new StdClass();
+
+$company->company = "WELL";
+$company->ceo = $ceo;
+$company->manager = new StdClass();
+$company->manager->name = "Giovanne";
+$company->manager->mail = "curso@upinside.com";
+
+var_dump($company);
 
 /**
  * [ análise ] class | objetcs | instances
  */
 fullStackPHPClassSession("análise", __LINE__);
+
+$date = new DateTime();
+
+var_dump([
+    "class" => get_class($date),
+    "methods" => get_class_methods($date),
+    "vars" => get_object_vars($date),
+    "parent" => get_parent_class($date),
+    "subclass" => is_subclass_of($date, "DateTime")
+]);
+
+$exception = new PDOException();
+
+var_dump([
+    "class" => get_class($exception),
+    "methods" => get_class_methods($exception),
+    "vars" => get_object_vars($exception),
+    "parent" => get_parent_class($exception),
+    "subclass" => is_subclass_of($exception, "Exception")
+]);
