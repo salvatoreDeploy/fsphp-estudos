5a4234a77841fb582d076b62d16dc6a8c7dc2fe8
salvatoreDeploy <liderhenrique@gmail.com>
2024-09-01 21:43:45 -0300

    new: Classe de datas

--- 03-manipulacao-e-tratamento/03-05-manipulacao-de-datas/index.php
+++ 03-manipulacao-e-tratamento/03-05-manipulacao-de-datas/index.php
@@ -9,8 +9,42 @@ fullStackPHPClassName("03.05 - Manipulação de datas");
  */
 fullStackPHPClassSession("a função date", __LINE__);
 
+var_dump([
+    date_default_timezone_get(),
+    date(DATE_W3C),
+    date("d/m/Y h:i:s")
+]);
+
+define("DATE_BR", "d/m/Y H:i:s");
+define("DATE_TIMEZONE", "Pacific/Apia");
+
+date_default_timezone_set(DATE_TIMEZONE);
+var_dump([
+    date_default_timezone_get(),
+    date(DATE_BR)
+]);
+
+var_dump(getdate());
+
+echo "<p>Hoje é dia ", getdate()["mday"], "!</p>";
 
 /**
  * [ string to date ] strtotime | strftime
  */
 fullStackPHPClassSession("string to date", __LINE__);
+
+var_dump([
+    "strtotime NOW" => strtotime("now"),
+    "time" => time(),
+    "strtotime+10d" => strtotime("+10days"),
+    "date DATE_BR" => date(DATE_BR),
+    "date +10days" => date(DATE_BR, strtotime("+10days")),
+    "date -10days" => date(DATE_BR, strtotime("-10days")),
+    "date +1year" => date(DATE_BR, strtotime("+1year")),
+    "date +2years" => date(DATE_BR, strtotime("+2years"))
+]);
+
+$format = "%d/%m/%Y %Hh%M minutos";
+
+echo "<p>A data de hoje é ", strftime($format), "</p>";
+echo strftime("<p>Hoje é dia %d do mes %m de %Y as %H horas e %m minutos");
--- 03-manipulacao-e-tratamento/03-06-uma-classe-datetime/index.php
+++ 03-manipulacao-e-tratamento/03-06-uma-classe-datetime/index.php
@@ -7,6 +7,35 @@ fullStackPHPClassName("03.06 - Uma classe DateTime");
  */
 fullStackPHPClassSession("A classe DateTime", __LINE__);
 
+define("DATE_BR", "d/m/Y H:i:s");
+
+$dateNow = new DateTime();
+$dateBirth = new DateTime("1991-05-12");
+$dateStatic = DateTime::createFromFormat(DATE_BR, "10/03/2024 12:00:00");
+$dateStaticNotFormat = DateTime::createFromFormat("d/m/Y", "12/07/2024");
+
+var_dump([
+    $dateNow,
+    $dateBirth,
+    $dateStatic,
+    $dateStaticNotFormat
+]);
+
+var_dump([
+    $dateNow->format(DATE_BR),
+    $dateNow->format("d")
+]);
+
+echo "<p>Hoje é dia {$dateNow->format("d")} do {$dateNow->format("m")} de {$dateNow->format("Y")}</p>";
+
+$newTimeZone = new DateTimeZone("Pacific/Apia");
+$newDateTime = new DateTime("now", $newTimeZone);
+
+var_dump([
+    $newTimeZone,
+    $newDateTime,
+    $dateNow
+]);
 
 /*
  * [ DateInterval ] http://php.net/manual/en/class.dateinterval.php
@@ -14,8 +43,53 @@ fullStackPHPClassSession("A classe DateTime", __LINE__);
  */
 fullStackPHPClassSession("A classe DateInterval", __LINE__);
 
+$dateInterval = new DateInterval("P10Y2MT10M");
+
+var_dump([$dateInterval]);
+
+$dateTime = new DateTime("now");
+$dateTime->add($dateInterval);
+$dateTime->sub($dateInterval);
+
+var_dump([$dateTime]);
+
+$birth = new DateTime(date("Y") . "-05-12");
+
+$diff = $dateNow->diff($birth);
+
+var_dump($diff);
+
+if($diff->invert){
+    echo "<p>Seu aniversario foi a {$diff->days} dias</p>";
+}else{
+    echo "<p>Faltan {$diff->days} dias para seu aniversario</p>";
+}
+
+$dataResources = new DateTime("now");
+var_dump([
+    $dataResources->format(DATE_BR),
+    $dataResources->sub(DateInterval::createFromDateString("10days"))->format(DATE_BR),
+    $dataResources->add(DateInterval::createFromDateString("20days"))->format(DATE_BR)
+]);
 
 /**
  * [ DatePeriod ] http://php.net/manual/pt_BR/class.dateperiod.php
  */
 fullStackPHPClassSession("A classe DatePeriod", __LINE__);
+
+$start = new DateTime("now");
+$interval = new DateInterval("P1M");
+$end = new DateTime("01-01-2025");
+
+$period = new DatePeriod($start, $interval, $end);
+
+var_dump([
+    $start->format(DATE_BR),
+    $interval,
+    $end->format(DATE_BR)
+]);
+
+echo "<h1>Sua Assinatura:</h1>";
+foreach ($period as $recurrences){
+    echo "<p>Proximo vencimento: {$recurrences->format(DATE_BR)}</p>";
+}
